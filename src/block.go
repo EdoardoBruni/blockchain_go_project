a6064a13ee8c7f6e8ea0a3a12982edbcd4efc1b0
edoardo.bruni <edoardo.bruni91@gmail.com>
2018-06-22 15:23:27 +0200

    aggiunta src e block.go al repository

---
+++ src/block.go
@@ -0,0 +1,16 @@
+func (b *Block) SetHash() {
+
+	timestamp := []byte(strconv.FormatInt(b.Timestamp,10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+
